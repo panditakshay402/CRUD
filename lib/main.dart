54c68e0edf2dca11e1ff6408d2f862346f02378f
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-26 23:12:13 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(CRUD());
+}
+
+class CRUD extends StatelessWidget {
+  const CRUD({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
