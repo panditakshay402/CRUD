066beefc66d8309c146da6e1a56d27fad36c404c
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-29 23:06:17 +0530

    backend tried

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(Crud());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const Crud());
 }
 
 class Crud extends StatelessWidget {
