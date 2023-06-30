0ae1ecf4a6201b970bb2ba39b78df88b9af7684c
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-06-30 15:24:10 +0530

    backend tried 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
